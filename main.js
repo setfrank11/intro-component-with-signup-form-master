981c3fcd4bf888ff1d0612d1f7b6c0ebf1782941
setfrank11 <105731465+setfrank11@users.noreply.github.com>
2023-08-28 22:22:02 -0500

    agregando arcchivos de html, css, sass y js, de formulario de verifacion con email y password

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName = document.getElementById('firstNameInput');
+const lastName = document.getElementById('lastNameInput');
+const emailAddress = document.getElementById('emailInput');
+const password = document.getElementById('passInput');
+
+const firstError = document.getElementById('firstNameError');
+const lastError = document.getElementById('lastNameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passError');
+
