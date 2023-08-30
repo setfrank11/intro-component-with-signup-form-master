215935a0426f6b5b5c99f3ec50aa917b6e2849b7
setfrank11 <105731465+setfrank11@users.noreply.github.com>
2023-08-30 12:27:25 -0500

    agegando funciones de js para validar formulario

--- main.js
+++ main.js
@@ -8,3 +8,40 @@ const lastError = document.getElementById('lastNameError');
 const emailError = document.getElementById('emailError');
 const passwordError = document.getElementById('passError');
 
+const button = document.querySelector('.submit');
+
+button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    validateEmpty(firstName.value, firstName, firstError, 'First Name cannot be empty');
+    validateEmpty(lastName.value, lastName, lastError, 'Last Name cannot be empty');
+    validateEmail(emailAddress.value, emailAddress, emailError);
+    validateEmpty(password.value, password, passError, 'Password cannot be empty');
+});
+
+function validateEmail(valueInput, divInput, divError){
+    let regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    if(regExp.test(valueInput) == true){
+        hideError(divInput, divError);
+    } else {
+        showError(divInput, divError, 'looks like this is not an email')
+    }
+}
+
+function validateEmpty(valueInput, divInput, divError, nameInput){
+    if(valueInput.length == 0){
+        showError(divInput, divError, nameInput);
+    } else {
+        hideError(divInput, divError);
+    }
+};
+
+function showError(divInput, divError, error){
+    divInput.style.border = '1px solid red'
+    divError.innerHTML = `<img class="icon-error" src="./images/icon-error.svg" alt="">
+    <p class= "error">${error}</p>`;
+};
+
+function hideError(divInput, divError){
+    divInput.style.border = '1px solid hsl(246, 25%, 77%)';
+    divError.innerHTML = ``;
+};
